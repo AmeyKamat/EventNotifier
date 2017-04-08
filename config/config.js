a19481164fb5b90c43b364497c96f4cf583227c5
AmeyKamat <ask5332@gmail.com>
2017-04-08 20:43:19 +0530

    Initial Commit

--- config/config.js
+++ config/config.js
@@ -4,11 +4,7 @@ module.exports = {
 	    fromAddress: 'eventnotifier@ameykamat.in',
 	    fromName: 'Event Notifier',
 	    subject: "Today's Events",
-	    user: 'eventnotifier@ameykamat.in',
-	    password: 'auskamat',
-	    host: 'smtp.zoho.com',
-	    port: 465,
-	    domain: "sandboxac75089cba5a43cd9970c86b973e3ca1.mailgun.org",
-	    apiKey: "key-6ecbdd2a522b182c562227dc7a03162d"
+	    domain: "",
+	    apiKey: ""
 	}
 }
